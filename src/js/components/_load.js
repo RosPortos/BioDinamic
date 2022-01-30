74d9bf629d24109324f7b37c817e0967b0ce96bb
dreamnoob <vitaliklio1100@gmail.com>
2022-01-30 17:41:50 +0200

    add loader

---
+++ src/js/components/_load.js
@@ -0,0 +1,9 @@
+new Vivus('my-svg', {
+    type: 'delayed',
+    duration: 900,
+    animTimingFunction: Vivus.EASE_OUT,
+},
+);
+
+const svg = document.querySelector('.load__svg');
+svg.classList.add('animated');
