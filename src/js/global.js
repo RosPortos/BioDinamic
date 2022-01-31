9fc715a24a8af08a7ac0464b7cd03098f7013155
Rostislav <nehaiki@ukr.net>
2022-01-31 23:00:23 +0200

    add tabs sec

--- src/js/global.js
+++ src/js/global.js
@@ -1,6 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+    $('.tab').on('click', function (event) {
+        event.preventDefault();
 
+        $($(this).siblings()).removeClass('tab-active');
+        $($(this).closest('.tabs-wrapper').siblings().find('div')).slideUp(300).removeClass('tabs-content-active');
+
+        $(this).addClass('tab-active');
+        $($(this).attr('href')).slideDown(300).addClass('tabs-content-active');
+    });
 
 
 
